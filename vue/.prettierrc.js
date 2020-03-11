60ab222a5d1770963465f0060f926d9d9f3b01ce
Shabatin Viktor <shabatin.viktor@gmail.com>
2020-03-11 13:09:43 +0200

    vue lint

---
+++ vue/.prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "tabWidth": 2,
+  "semi": true,
+  "trailingComma": "all",
+  "singleQuote": true,
+  "bracketSpacing": false,
+  "vueIndentScriptAndStyle": false,
+  "printWidth": 80
+};
